41bccc1ca09aeb9a278a0829650413328aa83c9d
Martingarzon2004 <141690600+Martingarzon2004@users.noreply.github.com>
2023-10-13 12:32:24 -0500

    Se añadieron los archivos del proyecto API alternativo.

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const morgan=require('morgan');
+const cors=require('cors');
+const app=express();
+const{mongoose}=require('./database');
+app.set('port',process.env.PORT||3000);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors({origin:'http://localhost:4200'}));
+app.use('/api/inventarios',require('./routes/inventario.routes'));
+app.listen(app.get('port'),()=>{
+    console.log('server activo en el puerto',app.get('port'));
+});
